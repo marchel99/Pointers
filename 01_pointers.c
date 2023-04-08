1da4df68e3b437a159aa10a62dc432c362be0171
marchel99 <kubamarchelski@live.com>
2023-04-08 17:28:10 +0200

    On branch master  Your branch is behind 'origin/master' by 1 commit, and can be fast-forwarded.    (use "git pull" to update your local branch)

--- 01_pointers.c
+++ 01_pointers.c
@@ -11,11 +11,12 @@ int *pa;
 pa=&a;
 
 printf("Variable: %d, Pointer that points to that variable:%d\n",a,*pa);
+
+printf("Address of variable: %d, Address of pointer:%d\n",pa,&pa);
+
+printf("Size of variable: %d, Size of pointer:%d\n",sizeof(a),sizeof(pa));
+
 printf("%d",a==*pa);
-
-
-
-
 
     return 0;
 }
