990eb31783f2ae05e85443d6c7a371ddfb4972d6
marchel99 <kubamarchelski@live.com>
2023-04-10 23:31:29 +0200

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   zadaniew_c.c
    	new file:   zadaniew_c.exe

--- zadaniew_c.c
+++ zadaniew_c.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 float dodaj(float a, float b);
 float odejmij(float a, float b);
 float mnoz(float a, float b);
@@ -8,34 +7,38 @@ float dziel(float a, float b);
 
 
 
-//ds
-
-
 typedef float (*pf)(float, float); // typedef dla wskaźnika na funkcję
 
 int main() {
+
+
     pf dzialania[4] = {dodaj, odejmij, mnoz, dziel}; // tablica wskaźników na funkcje
     
-    //d
-    int x;
-    float a, b;
-    printf("Podaj wybor dzialania (0-dodaj, 1-odejmij, 2-mnoz, 3-dziel): ");
-    scanf("%d", &x);
-    printf("Podaj dwa argumenty: ");
-    scanf("%f %f", &a, &b);
-    
-    printf("Wynik dzialania to: %f\n", dzialania[x](a, b));
-    
+
+
+    printf("Wynik dzialania to: %f\n", dzialania[1](2, 3));
+
+
+
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 float dodaj(float a, float b) {
     return a + b;  
 }
-//a
-
-int b=0;
-
 
 float odejmij(float a, float b) {
     return a - b;
