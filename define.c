3fe75adb3056e6820fe8ad9db0646c500a99b669
marchel99 <kubamarchelski@live.com>
2023-04-11 00:18:36 +0200

    On branch master  Your branch is up to date with 'origin/master'.

--- define.c
+++ define.c
@@ -1,43 +1,12 @@
 #include <stdio.h>
 
-// function declarations
-int add(int a, int b);
-int subtract(int a, int b);
-int multiply(int a, int b);
+#define ZMIENNA
 
 int main() {
 
 
  
-typedef int (*pt)(int,int);
-
-     pt func_ptr_array[3] = {add, subtract, multiply};
-
-
-   // int (*func_ptr_array[3])(int, int) = {add, subtract, multiply};
-   // to samo
-     
-
-    
-
-
-    // call functions using the array
-    int a = 10, b = 5;
-    printf("a + b = %d\n", (*func_ptr_array[0])(a, b));
-    printf("a - b = %d\n", (*func_ptr_array[1])(a, b));
-    printf("a * b = %d\n", (*func_ptr_array[2])(a, b));
 
     return 0;
 }
 
-// function definitions
-int add(int a, int b) {
-    return a + b;
-}
-
-int subtract(int a, int b) {
-    return a - b;
-}
-
-int multiply(int a, int b) {
-    return a * b;
