df36bcecbc248a83a9c0ec7ab010fdc6d5e547d0
marchel99 <kubamarchelski@live.com>
2023-06-27 20:29:48 +0200

    modified:   01_pointers.c

--- 01_pointers.c
+++ 01_pointers.c
@@ -10,6 +10,7 @@ int *pa;
 //basic declaration of pointer. It has to be an int, because it points to the int value
 pa=&a;
 
+
 printf("Variable: %d, Pointer that points to that variable:%d\n",a,*pa);
 
 printf("Address of variable: %d, Address of pointer:%d\n",pa,&pa);
